d6c7b09fb7ebd463bacdca700d0666b1d9c4cf1d
Pooja Pareek <pareek6155@gmail.com>
2025-09-11 15:44:13 +0530

    Made the changes in constant.js file

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -1,3 +1,7 @@
-export const BASE_URL = "/api";
+//export const BASE_URL = "/api";
 
-//export const BASE_URL = "http://localhost:8888";
+// export const BASE_URL = "http://localhost:8888";
+// console.log(location.hostname);
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:8888" : "/api";
