45b188fa0812178b9cb8bddc6b902d76f083ed8e
Pooja Pareek <pareek6155@gmail.com>
2025-09-13 20:04:49 +0530

    remove all the changes

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -1,11 +1,2 @@
-//export const BASE_URL = "/api";
-
-// export const BASE_URL = "http://localhost:8888";
-
-// export const BASE_URL =
-//   location.hostname === "localhost" ? "http://localhost:8888" : "/api";
-
 export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:8888"
-    : "http://13.60.20.59:8888";
+  location.hostname === "localhost" ? "http://localhost:8888" : "/api";
